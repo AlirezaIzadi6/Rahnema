a8430607d028416f3b3124866b5f77d50f96acb9
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-07-23 01:22:33 +0330

    Create type guard for expense

--- e2e/utilities.ts
+++ e2e/utilities.ts
@@ -1,7 +1,24 @@
+import {z} from "zod";
+import { expenseItemDto } from "../src/dto/expenseDto";
+
 export function isUserArray(data: User[]): data is User[] {
     return Array.isArray(data) && data.every(u => isUser(u));
 }
 
 export function isUser(data: User): data is User {
     return typeof(data.id) === "number" && typeof(data.name) === "string";
+}
+
+export function isExpense(data: unknown): data is Expense {
+    try {
+        const expenseZ = z.object({
+            creditorId: z.number(),
+            description: z.string().nonempty(),
+            debtors: z.array(expenseItemDto)
+        });
+        expenseZ.parse(data);
+        return true;
+    } catch {
+        return false;
+    }
 }
