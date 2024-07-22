0fb1522a22a7ffeba4c0cfe503dea132d795fb62
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-07-23 01:21:58 +0330

    Add expense route to app

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import {app as userRoutes} from "./route/user.route";
+import {app as expenseRoutes} from "./route/expense.route";
 
 export const app = express();
 
 app.use(express.json());
 
 app.use("/users", userRoutes);
+app.use("/expenses", expenseRoutes);
 app.use((req, res, next) => {
     res.status(404).send({message: "Not found"});
 });
