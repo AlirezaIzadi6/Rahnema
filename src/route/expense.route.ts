da075d64b310d271c25449943d6150a5ebf58710
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-07-23 01:21:14 +0330

    Update expense route

--- src/route/expense.route.ts
+++ src/route/expense.route.ts
@@ -1,11 +1,33 @@
 import {Router} from "express";
 import { ExpenseTracker } from "../expense-tracker";
-import { ExpenseDto } from "../dto/expenseDto";
+import { expenseDto, ExpenseDto } from "../dto/expenseDto";
+import { ZodError } from "zod";
 
+export const creategroupExpense = (data: unknown) => {
+    const newExpenseDto = expenseDto.parse(data);
+    const expense = {
+        
+    }
+}
 export const expenseTracker = new ExpenseTracker();
 
-export const createExpense = (expenseDto: ExpenseDto) => {
-    
+export const createExpense = (body: unknown): Expense => {
+    const newExpenseDto = expenseDto.parse(body);
+    const newExpense = expenseTracker.add(newExpenseDto);
+    return newExpense;
 }
+
 export const app = Router();
 
+app.post("/", (req, res) => {
+    try {
+        const expense = createExpense(req.body);
+        res.send(expense);
+    } catch(e) {
+        if (e instanceof ZodError) {
+            res.status(400).send({message: e.errors});
+            return;
+        }
+        res.status(500).send();
+    }
+})
