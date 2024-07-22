b4709ae8173a859da4d17a20777c8f575969a5a0
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-07-22 23:38:38 +0330

    Move app declaration and routing to api.ts file

---
+++ src/api.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import {app as userRoutes} from "./route/user.route";
+
+export const app = express();
+
+app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use((req, res, next) => {
+    res.status(404).send({message: "Not found"});
+});
+
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,4 @@
-import express from "express";
-import {app as userRoutes} from "./route/user.route";
-
-export const app = express();
-
-app.use(express.json());
-
-app.use("/users", userRoutes);
-app.use((req, res, next) => {
-    res.status(404).send({message: "Not found"});
-});
+import {app} from "./api";
 
 app.listen(3000, () => {
     console.log("Listening on port 3000...");
