b9167ab384cbb578b0e75647ac75912c9c15206b
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-07-26 18:31:03 +0330

    Add get-group-even endpoint

--- src/modules/route/expense.route.ts
+++ src/modules/route/expense.route.ts
@@ -6,6 +6,7 @@ import { ExpenseRepository } from "../expense/expense.repository";
 import { expenseDto } from "../expense/dto/expense-dto";
 import { HttpError } from "../../utilities/http-error";
 import { handleExpress } from "../../utilities/handle-express";
+import { groupId } from "../group/dto/group-dto";
 
 export const makeExpenseRouter = (expenseService: ExpenseService) => {
     const app = Router();
@@ -14,5 +15,10 @@ export const makeExpenseRouter = (expenseService: ExpenseService) => {
         const newExpenseDto = expenseDto.parse(req.body);
         handleExpress(res, () => expenseService.createExpense(newExpenseDto));
     })
+
+    app.get("/get-group-even/:id", (req, res) => {
+        const id = groupId.parse(req.params.id);
+        handleExpress(res, () => expenseService.getGroupEven(id));
+    });
     return app;
 }
